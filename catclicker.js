8f6c916e5121b659f34705b724e42d164c59d178
Marit Oliemans <maritoliemans@hotmail.com>
2018-03-28 17:21:02 -0600

    setting up catclicker.js for multiple cats

--- catclicker.js
+++ catclicker.js
@@ -1,7 +1,7 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable("Cat");
-	this.imgSrc = ko.observable("https://static1.squarespace.com/static/54e8ba93e4b07c3f655b452e/t/56c2a04520c64707756f4267/1493764650017/?format=500w");
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
 	this.level = ko.computed(function(){
 		if(this.clickCount() < 11){
 			return "NewBorn";
@@ -11,12 +11,18 @@ var Cat = function() {
 			return "Teen";
 		}
 	},this);
-	this.nicknames = ko.observableArray(["Spike", "Spikey", "Mr. Spike"]);
+	this.nicknames = ko.observableArray(data.nicknames);
 };
 
 
 var ViewModel = function() {
-	this.currentCat = ko.observable(new Cat());
+	this.currentCat = ko.observable(new Cat({
+		clickCount: 0,
+		name: "Cat",
+		imgSrc: "https://static1.squarespace.com/static/54e8ba93e4b07c3f655b452e/t/56c2a04520c64707756f4267/1493764650017/?format=500w",
+		nicknames: [["Spike", "Spikey", "Mr. Spike"]]
+
+	}));
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
