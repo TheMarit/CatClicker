2ca4fecc96a37196348de10acb0ae1c953bf6e3f
Marit Oliemans <maritoliemans@hotmail.com>
2018-03-28 16:56:26 -0600

    Separating out the model

--- catclicker.js
+++ catclicker.js
@@ -1,9 +1,8 @@
-var ViewModel = function() {
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable("Cat");
 	this.imgSrc = ko.observable("https://static1.squarespace.com/static/54e8ba93e4b07c3f655b452e/t/56c2a04520c64707756f4267/1493764650017/?format=500w");
 	this.level = ko.computed(function(){
-		console.log(this.clickCount())
 		if(this.clickCount() < 11){
 			return "NewBorn";
 		} else if (this.clickCount() < 101){
@@ -13,9 +12,16 @@ var ViewModel = function() {
 		}
 	},this);
 	this.nicknames = ko.observableArray(["Spike", "Spikey", "Mr. Spike"]);
+};
+
+
+var ViewModel = function() {
+	this.currentCat = ko.observable(new Cat());
+
 	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
 	};
 };
+
 
 ko.applyBindings(new ViewModel());
